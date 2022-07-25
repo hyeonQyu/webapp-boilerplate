20e23e188ec02c459931362b4681919337375c87
Hyeongyu Kim <dhk0561@naver.com>
2022-07-25 23:22:53 +0900

    chore: client 빌드 설정

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    exportPathMap: () => ({
+        '/': { page: '/' },
+    }),
+    images: {
+        loader: 'akamai',
+        path: '/',
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
 
-module.exports = nextConfig
+        return config;
+    },
+    assetPrefix: '.',
+};
+
+module.exports = nextConfig;
