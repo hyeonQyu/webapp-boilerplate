608dea9d08ce2ed16e057a8c9953f58228c000ed
hyeonQyu <dhk0561@naver.com>
2023-01-04 17:41:11 +0900

    chore: 리액트 쿼리 관련 기본 타입 정의

---
+++ apps/client/src/defines/reactQuery.ts
@@ -0,0 +1,18 @@
+import { UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+export interface UseQueryParams<Res, Req = undefined> {
+  req: Req;
+  queryOption?: UseQueryOptions<Res, AxiosError>;
+}
+
+export interface UseMutationParams<Res, Req> {
+  mutationOption?: UseMutationOptions<Res, AxiosError, Req>;
+}
+
+export const QUERY_KEY = {
+  directory: {
+    base: ['directory'] as const,
+    getDirectory: (path: string) => [...QUERY_KEY.directory.base, 'get', 'directory', path],
+  },
+};
